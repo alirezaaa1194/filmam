0bf07304fb6426aaf9c26340665a06d840d6ce06
Alireza <alirezaaa1194@gmail.com>
2026-02-21 03:44:51 +0330

    change swagger address

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,12 +1,46 @@
+// import 'dotenv/config';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import { ValidationPipe } from '@nestjs/common';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.setGlobalPrefix('api');
+
+//   const config = new DocumentBuilder()
+//     .setTitle('Filmam Api')
+//     .setVersion('1.0')
+//     .build();
+
+//   const document = SwaggerModule.createDocument(app, config);
+
+//   SwaggerModule.setup('docs', app, document);
+//   app.useGlobalPipes(new ValidationPipe());
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.setGlobalPrefix('api');
+
+  // سرو فایل‌های استاتیک Swagger
+  app.useStaticAssets(
+    join(__dirname, '..', 'node_modules', 'swagger-ui-dist'),
+    {
+      prefix: '/api/',
+    },
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Filmam Api')
@@ -15,8 +49,11 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('docs', app, document);
+  // Swagger روی /api
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
