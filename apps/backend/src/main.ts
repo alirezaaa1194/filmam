ec7ccd0fae50fc8948aa014905cc14ce81e374fb
Alireza <alirezaaa1194@gmail.com>
2026-02-21 04:07:17 +0330

    change swagger address

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,59 +1,30 @@
-// import 'dotenv/config';
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-// import { ValidationPipe } from '@nestjs/common';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.setGlobalPrefix('api');
-
-//   const config = new DocumentBuilder()
-//     .setTitle('Filmam Api')
-//     .setVersion('1.0')
-//     .build();
-
-//   const document = SwaggerModule.createDocument(app, config);
-
-//   SwaggerModule.setup('docs', app, document);
-//   app.useGlobalPipes(new ValidationPipe());
-//   await app.listen(process.env.PORT ?? 3000);
-// }
-// bootstrap();
-
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-
-  // سرو فایل‌های استاتیک Swagger
-  app.useStaticAssets(
-    join(__dirname, '..', 'node_modules', 'swagger-ui-dist'),
-    {
-      prefix: '/api/',
-    },
-  );
 
   const config = new DocumentBuilder()
     .setTitle('Filmam Api')
     .setVersion('1.0')
     .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-
-  // Swagger روی /api
-  SwaggerModule.setup('api', app, document);
-
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory, {
+    customCss: `
+      .swagger-ui .topbar { 
+        display: none !important; 
+      }
+    `,
+  });
   app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: ['https://filmamapp.ir'],
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
