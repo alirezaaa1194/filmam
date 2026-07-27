b4ae09f7738e3b76add876cd6f98dffd3a494c25
Alireza <alirezaaa1194@gmail.com>
2026-07-27 16:55:55 +0330

    Fix: Build bug

--- apps/admin/src/scripts/api.ts
+++ apps/admin/src/scripts/api.ts
@@ -1,34 +1,6 @@
 export const __Api = async (
-  url: string,
-  options: { method: string },
-  retry = true
-) => {
-//   return fetch(url, {
-//     ...options,
-//     headers: {
-//       'content-type': 'application/json',
-//       Authorization: `Bearer token`,
-//     },
-//   }).then((response) => {
-//     if (response.status === 401 && retry) {
-//       return fetch(refreshApi, {
-//         method: 'POST',
-//         headers: { 'content-type': 'application/json' },
-//       }).then((refreshResponse) => {
-//         if (refreshResponse.ok) {
-//           //save the new token to local storage or state management
+  _url: string,
+  _options: { method: string },
+  _retry = true
+) => {}
 
-//           // Retry the original request after refreshing the token
-//           return __Api(url, options, false)
-//         } else {
-//           // logout(); // Handle logout if refresh fails
-//           return refreshResponse
-//         }
-//       })
-//     } else if (response.status === 401 && !retry) {
-//       // logout(); // Handle logout if refresh fails
-//       return response
-//     }
-//     return response
-//   })
-}
--- apps/admin/src/types/index.ts
+++ apps/admin/src/types/index.ts
@@ -2,8 +2,204 @@ import type { __NavigateFn } from './navigate'
 import type { __Direction } from './direction'
 import type { __AuthUser, __AuthState } from './auth'
 import type { __Collapsible } from './layout'
+import type {
+  __PaginationType,
+  __JWTTokenType,
+  __MessageType,
+  __CardsDataType,
+  __MonthlyWatchDataType,
+  __RecentUserType,
+  __OverviewStatsType,
+  __AnalyticsCardsDataType,
+  __WeeklyChartDataType,
+  __TopMovieType,
+  __TopGenreType,
+  __AnalyticsStatsType,
+  __SummaryStatsType,
+  __CommentUserType,
+  __CommentType,
+  __AllCommentsType,
+  __EntityCommentType,
+  __ContactType,
+  __CountryTranslationType,
+  __CountryType,
+  __CountryListType,
+  __CreateCountryType,
+  __EpisodeFileType,
+  __EpisodeTranslationType,
+  __EpisodeDetailAdminType,
+  __EpisodeDetailSeasonType,
+  __EpisodeDetailNextEpisodeType,
+  __MovieDetailPublicType,
+  __EpisodeDetailPublicType,
+  __EpisodeListItemType,
+  __FactorTranslationType,
+  __FactorUploadType,
+  __FactorFileTypeDef,
+  __FactorProfileType,
+  __FactorType,
+  __FactorDetailType,
+  __FactorListType,
+  __FactorMovieListItemType,
+  __GenreTranslationType,
+  __GenreType,
+  __GenreListType,
+  __HeaderMenuTranslationType,
+  __HeaderMenuFilterType,
+  __HeaderMenuDetailType,
+  __HeaderMenuPublicType,
+  __HeaderMenuListType,
+  __LanguageTranslationType,
+  __LanguageType,
+  __LanguageListType,
+  __MovieFileType,
+  __MovieRoleType,
+  __MovieFactorType,
+  __MovieGenreType,
+  __MovieCountryType,
+  __MovieLanguageType,
+  __MovieSeasonType,
+  __MovieAdminFactorProfileType,
+  __MovieAdminFactorTranslationType,
+  __MovieAdminFactorType,
+  __MovieAdminGenreTranslationType,
+  __MovieAdminGenreType,
+  __MovieAdminSeasonType,
+  __MovieAdminEpisodeType,
+  __MovieAdminTranslationType,
+  __MovieAdminDetailType,
+  __MovieRecommendedType,
+  __MovieListItemType,
+  __PushSubscriptionType,
+  __RoleTranslationType,
+  __RoleType,
+  __RoleListType,
+  __SeasonFileType,
+  __SeasonType,
+  __SeasonTranslationType,
+  __SeasonDetailMovieTranslationType,
+  __SeasonDetailMovieType,
+  __SeasonDetailType,
+  __SeasonListType,
+  __SeasonEpisodeFileType,
+  __SeasonEpisodeListItemType,
+  __SectionFilterType,
+  __SectionTranslationType,
+  __SectionType,
+  __SectionDetailType,
+  __SectionMovieListItemType,
+  __SectionListItemType,
+  __TagTranslationType,
+  __TagType,
+  __TagListType,
+  __UploadType,
+  __UserType,
+  __UserMovieActionType,
+  __MovieBriefType,
+  __EpisodeBriefType,
+  __UserMovieListItemType,
+  __UserMovieActionsType,
+} from './general'
+import { __AppLanguagesEnum } from './general'
 
 export type { __NavigateFn as NavigateFn }
 export type { __Direction as Direction }
 export type { __AuthUser as AuthUser, __AuthState as AuthState }
 export type { __Collapsible as Collapsible }
+export type { __PaginationType as PaginationType }
+export type { __JWTTokenType as JWTTokenType }
+export type { __MessageType as MessageType }
+export type { __CardsDataType as CardsDataType }
+export type { __MonthlyWatchDataType as MonthlyWatchDataType }
+export type { __RecentUserType as RecentUserType }
+export type { __OverviewStatsType as OverviewStatsType }
+export type { __AnalyticsCardsDataType as AnalyticsCardsDataType }
+export type { __WeeklyChartDataType as WeeklyChartDataType }
+export type { __TopMovieType as TopMovieType }
+export type { __TopGenreType as TopGenreType }
+export type { __AnalyticsStatsType as AnalyticsStatsType }
+export type { __SummaryStatsType as SummaryStatsType }
+export type { __CommentUserType as CommentUserType }
+export type { __CommentType as CommentType }
+export type { __AllCommentsType as AllCommentsType }
+export type { __EntityCommentType as EntityCommentType }
+export type { __ContactType as ContactType }
+export type { __CountryTranslationType as CountryTranslationType }
+export type { __CountryType as CountryType }
+export type { __CountryListType as CountryListType }
+export type { __CreateCountryType as CreateCountryType }
+export type { __EpisodeFileType as EpisodeFileType }
+export type { __EpisodeTranslationType as EpisodeTranslationType }
+export type { __EpisodeDetailAdminType as EpisodeDetailAdminType }
+export type { __EpisodeDetailSeasonType as EpisodeDetailSeasonType }
+export type { __EpisodeDetailNextEpisodeType as EpisodeDetailNextEpisodeType }
+export type { __MovieDetailPublicType as MovieDetailPublicType }
+export type { __EpisodeDetailPublicType as EpisodeDetailPublicType }
+export type { __EpisodeListItemType as EpisodeListItemType }
+export type { __FactorTranslationType as FactorTranslationType }
+export type { __FactorUploadType as FactorUploadType }
+export type { __FactorFileTypeDef as FactorFileTypeDef }
+export type { __FactorProfileType as FactorProfileType }
+export type { __FactorType as FactorType }
+export type { __FactorDetailType as FactorDetailType }
+export type { __FactorListType as FactorListType }
+export type { __FactorMovieListItemType as FactorMovieListItemType }
+export type { __GenreTranslationType as GenreTranslationType }
+export type { __GenreType as GenreType }
+export type { __GenreListType as GenreListType }
+export type { __HeaderMenuTranslationType as HeaderMenuTranslationType }
+export type { __HeaderMenuFilterType as HeaderMenuFilterType }
+export type { __HeaderMenuDetailType as HeaderMenuDetailType }
+export type { __HeaderMenuPublicType as HeaderMenuPublicType }
+export type { __HeaderMenuListType as HeaderMenuListType }
+export type { __LanguageTranslationType as LanguageTranslationType }
+export type { __LanguageType as LanguageType }
+export type { __LanguageListType as LanguageListType }
+export type { __MovieFileType as MovieFileType }
+export type { __MovieRoleType as MovieRoleType }
+export type { __MovieFactorType as MovieFactorType }
+export type { __MovieGenreType as MovieGenreType }
+export type { __MovieCountryType as MovieCountryType }
+export type { __MovieLanguageType as MovieLanguageType }
+export type { __MovieSeasonType as MovieSeasonType }
+export type { __MovieAdminFactorProfileType as MovieAdminFactorProfileType }
+export type { __MovieAdminFactorTranslationType as MovieAdminFactorTranslationType }
+export type { __MovieAdminFactorType as MovieAdminFactorType }
+export type { __MovieAdminGenreTranslationType as MovieAdminGenreTranslationType }
+export type { __MovieAdminGenreType as MovieAdminGenreType }
+export type { __MovieAdminSeasonType as MovieAdminSeasonType }
+export type { __MovieAdminEpisodeType as MovieAdminEpisodeType }
+export type { __MovieAdminTranslationType as MovieAdminTranslationType }
+export type { __MovieAdminDetailType as MovieAdminDetailType }
+export type { __MovieRecommendedType as MovieRecommendedType }
+export type { __MovieListItemType as MovieListItemType }
+export type { __PushSubscriptionType as PushSubscriptionType }
+export type { __RoleTranslationType as RoleTranslationType }
+export type { __RoleType as RoleType }
+export type { __RoleListType as RoleListType }
+export type { __SeasonFileType as SeasonFileType }
+export type { __SeasonType as SeasonType }
+export type { __SeasonTranslationType as SeasonTranslationType }
+export type { __SeasonDetailMovieTranslationType as SeasonDetailMovieTranslationType }
+export type { __SeasonDetailMovieType as SeasonDetailMovieType }
+export type { __SeasonDetailType as SeasonDetailType }
+export type { __SeasonListType as SeasonListType }
+export type { __SeasonEpisodeFileType as SeasonEpisodeFileType }
+export type { __SeasonEpisodeListItemType as SeasonEpisodeListItemType }
+export type { __SectionFilterType as SectionFilterType }
+export type { __SectionTranslationType as SectionTranslationType }
+export type { __SectionType as SectionType }
+export type { __SectionDetailType as SectionDetailType }
+export type { __SectionMovieListItemType as SectionMovieListItemType }
+export type { __SectionListItemType as SectionListItemType }
+export type { __TagTranslationType as TagTranslationType }
+export type { __TagType as TagType }
+export type { __TagListType as TagListType }
+export type { __UploadType as UploadType }
+export type { __UserType as UserType }
+export type { __UserMovieActionType as UserMovieActionType }
+export type { __MovieBriefType as MovieBriefType }
+export type { __EpisodeBriefType as EpisodeBriefType }
+export type { __UserMovieListItemType as UserMovieListItemType }
+export type { __UserMovieActionsType as UserMovieActionsType }
+export { __AppLanguagesEnum as AppLanguagesEnum }
